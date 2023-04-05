8fb410c190e0fe4311729b5fa86750bc68a64f60
Moebirdie <93432701+Moebirdie@users.noreply.github.com>
2023-04-04 23:35:57 -0400

    adding 1st JS lesson to GitHub

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,34 @@
+
+//var topics = "HTML, CSS, GIT, Javascript";
+//console.log(topics);
+
 //basic if, then else.  A switch does the same, when do you choose?
 
-var topic = "HTML";
+//var topic = "HTML";
 
-if (topic === 'HTML') {
-  console.log("Let's study HTML!");
-} else if (topic === 'CSS') {
-  console.log("Let's study CSS!");
-} else if (topic === 'Git') {
-  console.log("Let's study Git!");
-} else if (topic === 'JavaScript') {
-  console.log("Let's study JavaScript!");
-} else {
-  console.log('Please try again!');
-}
+//if (topic === 'HTML') {
+//  console.log("Let's study HTML!");
+//} else if (topic === 'CSS') {
+//  console.log("Let's study CSS!");
+//} else if (topic === 'Git') {
+//  console.log("Let's study Git!");
+//} else if (topic === 'JavaScript') {
+//  console.log("Let's study JavaScript!");
+//} else {
+//  console.log('Please try again!');
+//}
 
 //looping over an array.  
 
-var shapes = ["triangle","square","pentagon","circle"];
-console.log(shapes[0])
+//var shapes = ["triangle","square","pentagon","circle"];
+//console.log(shapes[0])
 
+//for(x=0; x < shapes.length; x++) {
+//console.log(shapes[x]);
+//}
+
+var topics = ["HTML", "CSS", "GIT", "JS"];
+for(x = 0; x < topics.length; x++) {
+  console.log(topics[x]);
+}
 
